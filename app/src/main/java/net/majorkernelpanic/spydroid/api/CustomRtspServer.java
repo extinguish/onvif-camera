1217f4cc57bd397555f36e9fc05a039e01ba4f7e
guoshichao <shichao.guo@gmail.com>
2018-10-26 16:44:22 +0800

    guoshichao: initial commit;

---
+++ app/src/main/java/net/majorkernelpanic/spydroid/api/CustomRtspServer.java
@@ -0,0 +1,12 @@
+package net.majorkernelpanic.spydroid.api;
+
+import net.majorkernelpanic.streaming.rtsp.RtspServer;
+
+public class CustomRtspServer extends RtspServer {
+    public CustomRtspServer() {
+        super();
+        // RTSP server disabled by default
+        mEnabled = false;
+    }
+}
+
